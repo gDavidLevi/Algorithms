a23513e2023a4dcd65035e65f64bcbf23ca985d6
David A. Levi <gDavidLevy@gmail.com>
2018-08-29 11:11:20 +0300

    lesson1

--- src/ru/davidlevi/lesson1/hw/MainClass.java
+++ src/ru/davidlevi/lesson1/hw/MainClass.java
@@ -1,14 +1,18 @@
-package ru.davidlevi.lesson1;
+package ru.davidlevi.lesson1.hw;
 
 public class MainClass {
     public static void main(String[] args) {
         /*
          Домашняя работа № 1
+
+         См. https://habr.com/post/188010/
          */
         int[] arrayInteger = {13, 24, 35, 7, 14, 20, 54, -7, -15, 10, 31};
+        System.out.println(SimplestAlgorithms.pow(2f, 6)); // 64.0
         System.out.println(SimplestAlgorithms.quickPow(2f, 6)); // 64.0
+        System.out.println(SimplestAlgorithms.powRecursion(2f, 6)); // 64.0
         System.out.println(SimplestAlgorithms.findMin(arrayInteger)); // -15
-        System.out.println(SimplestAlgorithms.findMinRec(arrayInteger[arrayInteger.length - 1], arrayInteger, arrayInteger.length - 1)); // -15
+        System.out.println(SimplestAlgorithms.findMinRecursion(arrayInteger[arrayInteger.length - 1], arrayInteger, arrayInteger.length - 1)); // -15
         System.out.println(SimplestAlgorithms.average(arrayInteger)); // 16.90909
     }
 }
---
+++ src/ru/davidlevi/lesson1/hw/SimplestAlgorithms.java
@@ -0,0 +1,109 @@
+package ru.davidlevi.lesson1.hw;
+
+/**
+ * Список функций:
+ * pow - Возведение в степень
+ * quickPow - Возведение в степень используя чётность степени
+ * powRecursion - Возведение в степень рекурсивно
+ * findMin - Поиск минимального элемента
+ * findMinRecursion - Поиск минимального элемента рекурсивно
+ * average - Нахождение среднего арифметического
+ */
+class SimplestAlgorithms {
+
+    /**
+     * Возведение в степень
+     * Сложность: O(n)
+     *
+     * @param base   float
+     * @param degree int
+     * @return double
+     */
+    static double pow(float base, int degree) {
+        int result = 1;
+        int i = 0;
+        while (i < degree) {
+            result *= base;
+            i++;
+        }
+        return result;
+    }
+
+    /**
+     * Возведение в степень используя чётность степени
+     * Сложность: O(log n)
+     *
+     * @param base   основание
+     * @param degree степень
+     * @return double
+     */
+    static double quickPow(float base, int degree) {
+        if (degree == 0) return 1;
+        float result = 1;
+        while (degree > 0) {
+            if (degree % 2 == 0) {
+                degree /= 2;
+                base *= base;
+            } else {
+                degree -= 1;
+                result *= base;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Возведение в степень рекурсивно
+     * Сложность: O(n)
+     *
+     * @param base   float
+     * @param degree int
+     * @return double
+     */
+    static double powRecursion(float base, int degree) {
+        if (degree == 0) return 1;
+        return powRecursion(base, --degree) * base;
+    }
+
+    /**
+     * Поиск минимального элемента
+     * Сложность: O(n)
+     *
+     * @param array int[]
+     * @return int
+     */
+    static int findMin(int[] array) {
+        int min = array[0];
+        for (int element : array)
+            if (element < min) min = element;
+        return min;
+    }
+
+    /**
+     * Поиск минимального элемента рекурсивно
+     * Сложность: O(n)
+     *
+     * @param min   int
+     * @param array int
+     * @param index int
+     * @return int
+     */
+    static int findMinRecursion(int min, int array[], int index) {
+        if (index < 0) return min;
+        if (min > array[index]) min = array[index];
+        return findMinRecursion(min, array, index - 1);
+    }
+
+    /**
+     * Нахождение среднего арифметического
+     * Сложность: O(n)
+     *
+     * @param array int[]
+     * @return float
+     */
+    static float average(int[] array) {
+        int sum = 0;
+        for (int element : array) sum += element; // или int sum = Arrays.stream(array).sum();
+        return sum / (float) array.length;
+    }
+}
