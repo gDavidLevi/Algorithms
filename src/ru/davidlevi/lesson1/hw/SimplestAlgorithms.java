0ed9ca4b5112ec5171fa14e3dbab7ac4956c692d
David A. Levi <gDavidLevy@gmail.com>
2018-08-30 12:59:45 +0300

    lesson1

--- src/ru/davidlevi/lesson1/hw/SimplestAlgorithms.java
+++ src/ru/davidlevi/lesson1/hw/SimplestAlgorithms.java
@@ -326,19 +326,13 @@ class SimplestAlgorithms {
         int right = array.length - 1;
         do {
             for (int i = left; i < right; i++) {
-                if (array[i] > array[i + 1]) {
-                    buff = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = buff;
-                }
+                if (array[i] > array[i + 1])
+                    swap(array, i, i + 1);
             }
             right--;
             for (int i = right; i > left; i--) {
-                if (array[i] < array[i - 1]) {
-                    buff = array[i];
-                    array[i] = array[i - 1];
-                    array[i - 1] = buff;
-                }
+                if (array[i] < array[i - 1])
+                    swap(array, i, i - 1);
             }
             left++;
         } while (left < right);
