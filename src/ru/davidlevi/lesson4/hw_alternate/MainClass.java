5406e1bf12aee8c42fc270dd6b5ee14bdc498c54
David A. Levi <gDavidLevy@gmail.com>
2018-09-09 18:11:06 +0300

    lesson4

--- src/ru/davidlevi/lesson4/hw_alternate/MainClass.java
+++ src/ru/davidlevi/lesson4/hw_alternate/MainClass.java
@@ -3,20 +3,7 @@ package ru.davidlevi.lesson4.hw_alternate;
 import ru.davidlevi.lesson4.cw.Cat;
 
 public class MainClass {
-    /**
-     * @param arr  int[]
-     * @param size int
-     * @return int
-     */
-    private static int linear(int[] arr, int size) {
-        if (size > 0) return Math.min(arr[size - 1], linear(arr, size - 1));
-        else return arr[size];
-    }
-
     public static void main(String[] args) {
-        int[] arr = {1, 2, 7, 6, 5, 4, 3, 2, -5, 3, 4, 5, 6};
-        System.out.println(linear(arr, arr.length));
-
         DoubleRelatedListAlt list = new DoubleRelatedListAlt();
         System.out.println(list);
         list.push(new Cat(1, "cat1"));
